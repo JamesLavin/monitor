8eb216c26d419cfe8cc4392b2316c64bab74f611
James Lavin <james@jameslavin.com>
2019-08-17 18:49:40 -0400

    implement memory (mem)

--- lib/monitor/impl.ex
+++ lib/monitor/impl.ex
@@ -1,2 +1,12 @@
 defmodule Monitor.Impl do
+  @spec mem :: [
+          {:allocated, number} | {:pct_used, float} | {:total, number} | {:worst_pid, any},
+          ...
+        ]
+  def mem do
+    {total, allocated, worst_pid} = :memsup.get_memory_data()
+    # mem = [pct_free: mem[:free_memory] / mem[:total_memory]] ++ mem
+    # mem = [pct_usage: 1 - mem[:pct_free]] ++ mem
+    [total: total, allocated: allocated, pct_used: allocated / total * 100, worst_pid: worst_pid]
+  end
 end
--- lib/monitor/server.ex
+++ lib/monitor/server.ex
@@ -2,17 +2,26 @@ defmodule Monitor.Server do
   use GenServer
   alias Monitor.Impl
 
+  @spec start_link(any) :: {:ok, pid}
   def start_link(state) do
-    {:ok, pid} = GenServer.start_link(__MODULE__, state, name: {:global, {__MODULE__, node()}})
+    {:ok, _pid} = GenServer.start_link(__MODULE__, state, name: {:global, {__MODULE__, node()}})
   end
 
+  @impl true
   @spec init(any) :: {:ok, any}
-  def init(state) do
+  def init(state \\ %{}) do
+    IO.inspect("Starting monitoring")
+    :application.start(:sasl)
+    :application.start(:os_mon)
     {:ok, state}
   end
 
-  def address do
-    GenServer.call(__MODULE__, :address)
+  def mem do
+    GenServer.call(monitor_pid(), :mem)
+  end
+
+  def mem(pid) when is_pid(pid) do
+    GenServer.call(pid, :mem)
   end
 
   @spec monitor_name :: {:global, {Monitor.Server, atom}}
@@ -20,13 +29,28 @@ defmodule Monitor.Server do
     {:global, {__MODULE__, node()}}
   end
 
+  @spec global_monitor_name(atom) :: {Monitor.Server, atom}
+  def global_monitor_name(node_name) when is_atom(node_name) do
+    {__MODULE__, node_name}
+  end
+
   @spec global_monitor_name :: {Monitor.Server, atom}
   def global_monitor_name do
     {__MODULE__, node()}
   end
 
+  @spec monitor_pid(atom) :: :undefined | pid
+  def monitor_pid(node_name) when is_atom(node_name) do
+    global_monitor_name(node_name) |> :global.whereis_name()
+  end
+
   @spec monitor_pid :: :undefined | pid
   def monitor_pid do
     global_monitor_name() |> :global.whereis_name()
+  end
+
+  @impl true
+  def handle_call(:mem, _from, state) do
+    {:reply, Impl.mem(), state}
   end
 end
